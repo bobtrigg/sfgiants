c82ed5b7f7cb8b9396f7073298d9a98f4009945d
Bob Trigg <bob@Roberts-MacBook-Pro.local>
2016-12-21 11:09:20 -0800

    Upgrade to Angular 1.6; tweak nav bar styling

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 var giantsControllers = angular.module('giantsControllers', []);
 
 giantsControllers.controller('GridController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
+  $http.get('js/data.json').then(function(data) {
 	$scope.players = data;
   });
 }]);
